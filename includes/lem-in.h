aa2768e755d5d380d2b1148087a440ba8b05a8b9
Kalilo <kalilohansmann@gmail.com>
2016-07-20 14:53:04 +0200

    Added basic struct layout.

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -13,6 +13,28 @@
 **Structures
 */
 
+typedef struct		s_link
+{
+	int				room;
+	struct s_link	*next;		
+}					t_link;
+
+typedef struct		s_room
+{
+	int				room;
+	int				x;
+	int				y;
+	int				no_ants;
+	t_link			*links;
+}					t_room;
+
+typedef struct		s_env
+{
+	int				no_ants;
+	int				no_roooms;
+	t_room			*start;
+}					t_env;
+
 /*
 **Prototypes
 */
