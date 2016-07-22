dd6f8a065a7099fe0ef17c0540690920a6703321
Eartha HANSMANN <ehansman@e5r7p5.jnb.42.fr>
2016-07-22 08:20:04 +0200

    test

--- Tests/t2.c
+++ Tests/t2.c
@@ -7,7 +7,7 @@ int	main(void)
 
 	write(1, "Hello World!", 12);
 	sleep(1);
-//	while (k++ < 12)
-//		write(1, "\b \b", 3);
+	while (k++ < 12)
+		write(1, "\b \b", 3);
 	write(1, "Nothing here.\n", 14);
 }
