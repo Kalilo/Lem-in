fd5fb7107d437e7fbd7eaed1a61fa4e0f460a9a5
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2016-07-24 07:36:02 +0200

    Added a display function.
    Need to edit the structs to manage ant names.

--- SRCS/write_solution.c
+++ SRCS/write_solution.c
@@ -1,9 +1,25 @@
 #include "lem-in.h"
 
+void	print_move(t_list *move)
+{
+	while (move)
+	{
+		ft_putchar("L");
+		ft_atoi(move->ant);
+		ft_putchar("-");
+		ft_atoi(move->room);
+		if (move->next != NULL)
+			ft_putchar(' ');
+		move = move->next;
+	}
+	ft_putchar('\n');
+}
+
 int     display_solution1(t_list *path, int ants)
 {
 	t_list	    *active;
     t_list      *inc;
+    int			act_ants;
 
     *active = NULL;
     while (ants > 0 || active != NULL)
@@ -17,9 +33,10 @@ int     display_solution1(t_list *path, int ants)
         if (ants > 0)
         {
             add_room_link(active, path->room);
+            //need to add a variable to room, to keep track of which ant is in it.
             ants--;
+            act_ants++;
         }
-        print_links(active);//debug temp
-        //if ()
+        print_move(active);
     }
 }
